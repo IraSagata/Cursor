d54985d50c22fcda56bc714ad17b82a75ac26769
IraSagata <irasagata@gmail.com>
2019-09-06 15:58:12 +0300

    small changes

--- HomeWork#5/app.js
+++ HomeWork#5/app.js
@@ -1,9 +1,11 @@
 //Task#1 A function which prints str after n seconds.
 
 function printTimeout(str, seconds) {
-  setTimeout(function(){ console.log(str); }, seconds * 1000);
+  setTimeout(function() {
+    console.log(str);
+  }, seconds * 1000);
 }
-printTimeout('Hello', 5)
+printTimeout("Hello", 5);
 
 //Task#2 A function which returns sum of all numbers from 1 to n using recursion.
 
@@ -14,38 +16,38 @@ function sumAll(n) {
   }
   return sum;
 }
-  
-console.log(sumAll(2))
-console.log(sumAll(4))
+
+console.log(sumAll(2));
+console.log(sumAll(4));
 
 //Task#4 A function which returns factorial of number using recursion.
 
-var factorial = function(n) {
-  if(n == 0) {
-      return 1
+const factorial = function(n) {
+  if (n == 0) {
+    return 1;
   } else {
-      return n * factorial(n - 1);
+    return n * factorial(n - 1);
   }
-}
- 
+};
+
 console.log(factorial(3));
 console.log(factorial(5));
 
 //Task3 A function which takes str and time in seconds as arguments, then every second it should count down from time to 0 and print current time to console when time equals to 0 it prints str
 
 function bombTimer(str, time) {
-  var timerId = setInterval(function() {
-      if (time > 0) {
-        console.log(time);
-        time = time - 1;
-      } else { 
-        console.log(str);  
-        clearInterval(timerId);
-      }
+  let timerId = setInterval(function() {
+    if (time > 0) {
+      console.log(time);
+      time = time - 1;
+    } else {
+      console.log(str);
+      clearInterval(timerId);
+    }
   }, 1000);
 }
 
-bombTimer('Boooom', 3);
+bombTimer("Boooom", 3);
 /*3
 2
 1
@@ -54,15 +56,14 @@ Boooom*/
 //Task5 Implement function from task №3 (bombTimer) using recursion and setTimeout.
 
 function bombTimer(str, time) {
-  var timerId = setTimeout(function() {
-      if (time > 0) {
-        console.log(time);
-        time = time - 1;
-        bombTimer(str, time)
-      } else { 
-        console.log(str);  
-      }
+  let timerId = setTimeout(function() {
+    if (time > 0) {
+      console.log(time);
+      bombTimer(str, (time = time - 1));
+    } else {
+      console.log(str);
+    }
   }, 1000);
 }
 
-bombTimer('Boooom', 3);
+bombTimer("Boooom", 3);
